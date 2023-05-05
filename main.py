799b27fadabf77272b6f18ad9e0bca588f0d5258
wanxtray <132686367+wanxtray@users.noreply.github.com>
2023-05-06 00:36:28 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+def ungkapkan_perasaan(nama):
+    print("Halo", nama + "!")
+    print("Saya ingin berbicara tentang perasaan saya.")
+    print("Saya merasa ... (ungkapkan perasaan Anda di sini)")
+    print("Itu adalah perasaan yang penting bagi saya, dan saya ingin Anda tahu tentang itu.")
+    print("Terima kasih telah mendengarkan. Apakah Anda memiliki sesuatu yang ingin Anda bagikan juga?")
+
+# Contoh penggunaan:
+nama_penerima = input("Masukkan nama penerima: ")
+ungkapkan_perasaan(nama_penerima)
